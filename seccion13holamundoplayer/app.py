8f7c0fc97937b5664f22cf3f8523a31c299fb40a
lionel-prats <lionel.prats.c@gmail.com>
2025-11-18 00:48:50 -0300

    17/11/25, commit #2

---
+++ seccion13holamundoplayer/app.py
@@ -0,0 +1,5 @@
+from seccion13holamundoplayer import player
+
+p = player.Player()
+p.play()
+p.stop()
---
+++ seccion13holamundoplayer/seccion13holamundoplayer/player.py
@@ -0,0 +1,29 @@
+"""
+Este es el modulo que incluye la clase
+de reproductor de musica
+"""
+
+class Player:
+    """
+    Esta clase crea un reproductor
+    de musica
+    """
+
+    def play(self, song):
+        """
+        Reproduce la cancion que recibio
+        como parametro 
+  
+        Parameters:
+        song (str): este es un string con el path de la cancion
+
+        Returns:
+        int: devuelve 1 se reproduce con exito, en caso de fracaso devuelve 0
+        """
+        
+        print("reroduciendo cancion")
+
+    def stop(self):
+        """
+        """
+        print("stopping")
